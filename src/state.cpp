ea8b4cae8657fab0001131abf2486e4e0e7afc24
Will Brown <will.brown@chip-diagnostics.com>
2025-02-28 17:10:43 -0500

    moving code because esp only runs main.cpp

--- src/state.cpp
+++ src/state.cpp
@@ -1,4 +1,5 @@
 /*
+
 #include <Arduino.h>
 
 
@@ -10,24 +11,18 @@ enum State {
 };
 
 // Define pins that have the external LEDS
-const int LED1_pin = 15; 
-const int LED2_pin = 16;
+const int LED_PIN_1 = 12; 
+const int LED_PIN_2 = 14;
 
 // Define the pin the button is on
-const int BUTTON_PIN = 17;
+const int BUTTON_PIN = 33;
 
 int counter = 0;
 
-// button handler function, if the button is pressed increment the state
-void readButton() {
-    if (digitalRead(BUTTON_PIN) == HIGH) {
-      updateCounter();
-    }
-  
-    // need delay for debouncing? 
-}
-  
-  // Simple counter for 4 values (0,1,2,3), wraps around
+int lastState = HIGH; // the previous state from the input pin
+int currentState; 
+
+// Simple counter for 4 values (0,1,2,3), wraps around
 void updateCounter() {
     if (counter < 3) {
       counter++;
@@ -36,28 +31,44 @@ void updateCounter() {
       counter = 0;
     }
 }
+// button handler function, if the button is pressed increment the state
+void readButton() {
+
+    currentState = digitalRead(BUTTON_PIN);
+    if (lastState == LOW && currentState == HIGH) {
+      Serial.println("Button Pressed!");
+      updateCounter();
+      delay(100); // delay for debouncing
+    }
+    lastState = currentState;
+}
+  
+
   
 void changeState() {
     // TODO: could track state instead of calling this every loop
     switch (counter) {
       case BOTH_OFF:
-        digitalWrite(LED1_pin, LOW);
-        digitalWrite(LED2_pin, LOW);
+        digitalWrite(LED_PIN_1, LOW);
+        digitalWrite(LED_PIN_2, LOW);
         break;
       
       case LED1_ON:
-        digitalWrite(LED1_pin, HIGH);
-        digitalWrite(LED2_pin, LOW);
+        digitalWrite(LED_PIN_1, HIGH);
+        digitalWrite(LED_PIN_2, LOW);
         break;
       
       case LED2_ON:
-        digitalWrite(LED1_pin, LOW);
-        digitalWrite(LED2_pin, HIGH);
+        digitalWrite(LED_PIN_1, LOW);
+        digitalWrite(LED_PIN_2, HIGH);
         break;
       
       case BOTH_ON:
-        digitalWrite(LED1_pin, HIGH);
-        digitalWrite(LED2_pin, HIGH);
+        digitalWrite(LED_PIN_1, HIGH);
+        digitalWrite(LED_PIN_2, HIGH);
+        break;
+
+      default:
         break;
     }
 }
@@ -66,10 +77,12 @@ void changeState() {
 void setup() {
   Serial.begin(9600);
   Serial.println("------- Hello FLX Solutions Project ----------");
-  Serial.println("TTGO T7 V1.4 Mini32 LED Blink Example");
+  Serial.println("State Machine LED Controller V0.1");
 
   // Set the LED pin as an output
-  pinMode(BUILTIN_LED, OUTPUT);
+  pinMode(BUTTON_PIN, INPUT_PULLUP);
+  pinMode(LED_PIN_1, OUTPUT);
+  pinMode(LED_PIN_2, OUTPUT);
 
 }
 
